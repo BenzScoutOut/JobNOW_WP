9406a102b3741c82412bbac27d345bb0f47d5754
BenzScoutout <benz@scoutout.net>
2020-03-17 09:29:34 +0700

    update job_card function

--- function.php
+++ function.php
@@ -1,5 +1,3 @@
-
-<?php 
 /* Custom Part-Time */
 function show_card_design(){
 
@@ -24,59 +22,85 @@ function show_card_design(){
 
 <link rel="stylesheet" href="https://unpkg.com/flickity@2.0/dist/flickity.min.css">
 <script src="https://unpkg.com/flickity@2.0/dist/flickity.pkgd.min.js"></script>
-	<div class="row">
-		<?php 
-	foreach ($bodyResponse as $key => $value) {
-		if($key < 9){
-			
 		
-		if($value["salaryType"] == "MONTHLY"){
-			$salaryType = "บาท / เดือน";
-		}else if($value["salaryType"] == "DAILY"){
-			$salaryType = "บาท / ชั่วโมง";
-		}else if($value["salaryType"] == "HOURLY"){
-			$salaryType = "บาท / ชั่วโมง";
-		}else{
-			$salaryType = "บาท / ชั่วโมง";
-		}
-		
-		if($value["jobExpireTimeLeftUnit"] == "day"){
-			$jobExpireType = "วัน";
-		}else{
-			$jobExpireType = "ชั่วโมง";
-		}
-		
-		?> 
-		<div class="col-12 col-md-6 col-lg-4 d-none d-md-flex">
-			<a href="https://job.jobnow.work/job/<?php echo $value["uuid"];?>?utm_source=ScoutOut&utm_medium=JobNOW_LD_Parttime" target="_blank" class="mx-auto">
-			<div class="card-job mx-auto">
-				<img src="<?php echo $value["jobProfileImageUrl"]; ?>" class="image-job">
-				<div class="text-control d-flex flex-column">
-					<span class="job-title"><?php echo $value["jobTitle"]; ?></span>
-					<span class="salary-text"><?php echo number_format($value["salaryFrom"], 0, '', ','); ?> - <?php echo number_format($value["salaryTo"], 0, '', ','); ?> <?php echo $salaryType; ?></span>
-					<span class="company-text"><?php echo $value["headquarterName"]; ?></span>
-					<div class="row mt-auto">
-						<div class="col-lg-6 col-12 d-flex">
-							<span class="company-text">เหลืออีก <?php echo $value["jobExpireTimeLeft"]; ?> <?php echo $jobExpireType ?> </span>
-						</div>
-						<div class="col-lg-6 col-12 d-flex">
-							<button class="btn btn-apply mx-auto my-auto">
-								สมัครงานนี้
-							</button>
-						</div>
+		<div class="container d-none d-md-block mx-auto row-width">
+					  <div class="carousel" data-flickity='{ "wrapAround": false, "contain": true }'>
+						<?php
+						foreach ($bodyResponse as $key => $value2) {
+							
+										if($value2["salaryType"] == "MONTHLY"){
+											$salaryType = "บาท / เดือน";
+										}else if($value2["salaryType"] == "DAILY"){
+											$salaryType = "บาท / ชั่วโมง";
+										}else if($value2["salaryType"] == "HOURLY"){
+											$salaryType = "บาท / ชั่วโมง";
+										}else{
+											$salaryType = "บาท / ชั่วโมง";
+										}
+
+										if($value2["jobExpireTimeLeftUnit"] == "day"){
+											$jobExpireType = "วัน";
+										}else{
+											$jobExpireType = "ชั่วโมง";
+										}
+
+								?> 
+						  
+						  
+						
+										<div class="carousel-cell card-job mx-auto">
+											<a href="https://job.jobnow.work/job/<?php echo $value2["uuid"];?>?utm_source=ScoutOut&utm_medium=JobNOW_LD_Parttime" target="_blank" class="mx-auto">
+												<img src="<?php echo $value2["jobProfileImageUrl"]; ?>" class="image-job">
+												<div class="text-control d-flex flex-column">
+													<span class="job-title"><?php echo $value2["jobTitle"]; ?></span>
+													<div class="d-flex">
+														
+														<img src="https://jobnow.work/wp-content/uploads/2020/03/salary-icon@3x.png" class="img-salary my-auto">
+														<span class="salary-text my-auto"><?php echo number_format($value2["salaryFrom"], 0, '', ','); ?> - <?php echo number_format($value2["salaryTo"], 0, '', ','); ?> <?php echo $salaryType; ?></span>
+													</div>
+													
+													<div class="d-flex">
+														
+														<img src="https://jobnow.work/wp-content/uploads/2020/03/place-icon@3x.png" class="img-salary my-auto">
+													<span class="company-text my-auto"><?php echo $value2["headquarterName"]; ?></span>
+													</div>
+													<div class="d-flex">
+														<img src="https://jobnow.work/wp-content/uploads/2020/03/expired-icon@3x.png" class="img-salary my-auto">
+															<span class="company-text my-auto">เหลืออีก <?php echo $value2["jobExpireTimeLeft"]; ?> <?php echo $jobExpireType ?> </span>
+														</div>
+													<div class="row mt-auto">
+														
+														<div class="col-12 d-flex">
+															<button class="btn btn-apply mx-auto my-auto">
+																ดูรายละเอียดงาน
+															</button>
+														</div>
+													</div>
+												</div>
+											</a>
+										</div>
+						  <?php 
+							if($key == sizeof($bodyResponse)-1){ ?>
+								  <div class="carousel-cell card-job mx-auto">
+							  
+												<img src="https://jobnow.work/wp-content/uploads/2020/03/extra-card@3x.png" class="image-job">
+								  <div class="text-control margin-extra d-flex flex-column">
+										<span class="job-title d-flex">มีงานดีๆ ที่เหมาะกับคุณ</span>
+									  	<span class="job-title d-flex">รอคนอยู่อีกเยอะที่ <img src="https://jobnow.work/wp-content/uploads/2020/03/logo-text@3x.png" class="extra-card-logo-jobnow"></span>
+								  		<a href="https://job.jobnow.work/jobs?latitude=&longitude=&part_time=true&utm_source=ScoutOut&utm_medium=JobNOW_LD_Parttime" target="_blank" >
+											<button class="btn btn-apply-extra">
+																ดูงานใหม่ทั้งหมด
+											</button></a>
+									  <a href="https://account.scoutout.co/login?callbackUrl=https:%2F%2Fjob.jobnow.work%2Fredirect-authenticate%3Fredirect_path%3D%252Fprofile%26utm_source%3DScoutOut%26utm_medium%3DJobNOW_LD_Parttime%26utm_campaign%3D%26utm_content%3D%26fbclid%3D%26gclid%3D" target="_blank" class="job-title-red mx-auto">
+									  ฝากประวัติของคุณ
+									  </a>
+									  	
+								  </div>
+						  </div>
+							<?php } 
+								}?>
+						   </div>
 					</div>
-					
-				</div>
-				
-			</div>
-				</a>
-		</div>
-		
-		<?php
-		}
-	}?>
-		
-	</div>
 		<div class="container-fluid d-flex d-md-none mx-auto row-width">
 					  <div class="carousel w-100" data-flickity>
 						<?php
